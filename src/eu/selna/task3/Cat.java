6adc669f094c255cb2eb7a4d5f30968499ee0bc2
scokpius <scokpius@gmail.com>
2021-01-26 18:45:58 +0300

    working with while and for loops

---
+++ src/eu/selna/task3/Cat.java
@@ -0,0 +1,31 @@
+package eu.selna.task3;
+
+class Cat {
+    private String nameCat = "Barsik";
+    private int oldCat = 2;
+
+     Cat() {
+    }
+
+     Cat(String name, int old) {
+        nameCat = name;
+        oldCat = old;
+    }
+
+     String getNameCat() {
+        return this.nameCat;
+    }
+
+     void setNameCat(String name) {
+        this.nameCat = name;
+    }
+
+     int getOldCat() {
+        return oldCat;
+    }
+
+     void setOldCat(int old) {
+        this.oldCat = old;
+    }
+
+}
---
+++ src/eu/selna/task3/Main.java
@@ -0,0 +1,48 @@
+package eu.selna.task3;
+
+
+public class Main {
+
+public static void main(String[] args) {
+        Cat cat, catti;
+        String[] names = {"Kyzya", "Vasya", "Asya", "Liza", "Tom", "Wolly", "Muha", "Sirius", "Fluff", "Snowball"};
+        int[] olds = {2, 8, 3, 6, 1, 7, 4, 2, 1, 3};
+        Cat[] cats = new Cat[5];
+        int i = 1;
+
+        while (i<=10){
+            cat = new Cat();
+            System.out.println("Name cat is " + cat.getNameCat() + ". Hi is " + cat.getOldCat() + ".");
+            i++;
+        }
+        System.out.println("------------------------------------------------");
+        for (int j = 0; j < 10 ; j++) {
+            cat = new Cat();
+            cat.setNameCat(names[j]);
+            cat.setOldCat(olds[j]);
+            System.out.println("Name cat is " + cat.getNameCat() + ". Hi is " + cat.getOldCat() + ".");
+        }
+        System.out.println("------------------------------------------------");
+        i = 0;
+        do {
+            cat = new Cat(names[i], olds[i]);
+            System.out.println("Name cat is " + cat.getNameCat() + ". Hi is " + cat.getOldCat() + ".");
+            i++;
+        } while (i<10);
+        System.out.println("------------------------------------------------");
+
+        for (int j = 0; j < 5; j++) {
+            cats[j] =  new Cat();
+            cats[j].setNameCat(names[j+3]);
+            cats[j].setOldCat(olds[j+2]);
+        }
+        for (Cat cat1 : cats) {
+            System.out.println("Name cat is " + cat1.getNameCat() + ". Hi is " + cat1.getOldCat() + ".");
+        }
+        System.out.println("------------------------------------------------");
+        System.out.println(cats[1].equals(cats[2]));
+        catti = new Cat(names[5], olds[8]);
+        System.out.println(catti.toString());
+    }
+
+}
